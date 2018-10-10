941daabe9e7d71e1e53c1226a7bf3cb62f62641b
iwazaki.y <memorycross@gmail.com>
2018-10-10 22:24:22 +0900

    年表ように変更

--- client/src/components/IndexPage.vue
+++ client/src/components/IndexPage.vue
@@ -1,31 +1,45 @@
 <template>
-  <div class="movies">
-    <h1>test</h1>
+  <!-- <div class="movies">
+    <h1>年表</h1>
     <div v-for="movie in movies" v-bind:key="movie.id" class="movie">
       <img v-bind:src="movie.poster" class="poster">
       <div>
         <strong>{{movie.name}}</strong>, <i>{{movie.director}}</i> [{{movie.year}}]
         <router-link :to="{ name: 'show', params: { id: movie.id }}">더보기</router-link>
       </div>
-    </div>
+    </div> -->
+
+  <div id="timeline">
+    <ul id="datas">
+      <li v-for="(value, key, index) in dates" :key="index">
+        <a href="#{{chrono.year}}">{{chrono.year}}</a>
+      </li>
+    </ul>
+    <ul id="datas">>
+      <li v-for="(value, key, index) in dates" :key="index" id="{{chrono.year}}">
+        <img src="images/{{chrono.images}}.png" width="256" height="256" />
+        <h1>{{chrono.year}}</h1>
+        <h2>{{chrono.title}}</h2>
+        <p>{{chrono.text}}</p>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-  created () {
-    this.$http.get('/api/movies')
-    .then((response) => {
-      this.movies = response.data
-    })
+  created() {
+    this.$http.get("/chrono").then(response => {
+      this.movies = response.data;
+    });
   },
-  name: 'hello',
-  data () {
+  name: "hello",
+  data() {
     return {
-      movies: []
-    }
+      dates: []
+    };
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
